ff1b9d802c969c3a0b4b4021083c03eface3341c
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2020-09-20 14:53:52 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -10,8 +10,7 @@ sudo yum install -y git
 sudo yum install -y unzip
 
 # Install SBT
-sudo curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-
-sbt-rpm.repo
+sudo curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 sudo yum -y install sbt
 
 # Install Docker
