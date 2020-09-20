33bb2495be6bbd956a29d42862c293c001badfbe
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2020-09-20 22:47:49 +0530

    Create prereq.sh

---
+++ prereq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install Java8
+sudo yum install -y java-1.8.0-openjdk.x86_64
+
+# Install wget, git and unzip
+sudo yum install -y wget
+sudo yum install -y git
+sudo yum install -y unzip
+
+# Install SBT
+sudo curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+sudo yum -y install sbt
+
+# Install Docker
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf repolist -v
+sudo dnf list docker-ce --showduplicates | sort -r
+sudo dnf install -y docker-ce-3:18.09.1-3.el7
+sudo systemctl start docker
