fa09dd62aa4e648d978326619773f4f0c5acda2f
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2020-09-28 16:25:59 +0530

    Update prereq_ubuntu.sh

--- prereq_ubuntu.sh
+++ prereq_ubuntu.sh
@@ -33,6 +33,5 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-
 wget https://get.jenkins.io/war-stable/2.249.1/jenkins.war
 nohup java -jar jenkins.war &
